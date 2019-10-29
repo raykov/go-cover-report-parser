d571ff5a8b48adc7d25ab3f19074e591c77fcd46
Andrii Raikov <raykov.andrey@gmail.com>
2019-10-29 16:35:27 +0100

    Move checker to separate func

--- main.go
+++ main.go
@@ -2,25 +2,40 @@ package main
 
 import (
 	"bufio"
+	"errors"
+	"io"
 	"log"
 	"os"
 	"strings"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		log.Fatalf("Usage: \n\n\t%s path_to_coverage.out \n\n", os.Args[0])
+	if len(os.Args) < 2 {
+		parts := strings.Split(os.Args[0], "/")
+		log.Fatalf("Usage: \n\n\t%s path_to_coverage.out \n\n", parts[len(parts) - 1])
 	}
 
-	coverReportFilePath := os.Args[1]
-
-	f, err := os.Open(coverReportFilePath)
+	f, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	defer f.Close()
 
-	r := bufio.NewReader(f)
+	err = checkCoverage(f)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func checkCoverage(reader interface{}) (err error) {
+	var r *bufio.Reader
+
+	switch reader.(type) {
+	case io.Reader:
+		r = bufio.NewReader(reader.(io.Reader))
+	default:
+		return errors.New("reader should satisfy io.Reader interface")
+	}
 
 	// Skip first line
 	//	mode: atomic
@@ -36,8 +51,10 @@ func main() {
 		coverage := strings.Split(strings.TrimSpace(line), " ")
 
 		if coverage[2] == "0" {
-			log.Fatal("Some lines are not covered")
+			return errors.New("some lines are not covered")
 		}
 	}
+
 	log.Println("100% coverage")
+	return nil
 }

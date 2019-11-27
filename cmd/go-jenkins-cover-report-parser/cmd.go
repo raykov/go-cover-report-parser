5ab5e7a2c905c50d671f2db572e5ab81301a25fc
Andrii Raikov <raykov.andrey@gmail.com>
2019-11-27 10:43:03 +0100

    Move main to the cmd dir. Commands are not part of package

--- cmd/go-jenkins-cover-report-parser/cmd.go
+++ cmd/go-jenkins-cover-report-parser/cmd.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"os"
+
+	coverReport "github.com/raykov/go-jenkins-cover-report-parser"
 )
 
 const usageMessage = "" +
@@ -31,25 +32,21 @@ var (
 	v = flag.Bool("v", false, "detailed coverage report")
 )
 
-var uncoveredError = errors.New("some lines are not covered")
-
 func main() {
 	setupFlags()
 
-	f, err := os.Open(*coverprofile)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer f.Close()
-
-	cov, err := parseCoverage(f)
-	if err != nil {
-		log.Fatal(err.Error())
+	params := coverReport.Options{
+		CoverProfile: *coverprofile,
+		Verbose: *verbose || *v,
+		MinimumExpectedCoverage: *minimumExpectedCoverage,
 	}
 
-	err = checkCoverage(cov)
-	if err == uncoveredError {
-		os.Exit(2)
+	err := coverReport.Execute(params)
+	if err != nil {
+		if err == coverReport.UncoveredError {
+			os.Exit(2)
+		}
+		log.Fatal(err)
 	}
 }
 
